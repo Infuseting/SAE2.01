450896434ea7698ad6e4b391b62dff1d36b6c2b2
gillotl241 <achille.gillot-letang@etu.unicaen.fr>
2025-05-15 10:05:39 +0200

    [FEATURES] Ajout du constructeur de la classe Fight qui ne fait qu'initier un joueur et un monstre pour l'instant Ajout de la fonction turn qui fait jouer un tour au joueur [FIX] Ajout du mot clé final aux attributs player et monster de la classe Fight qui sont censés être en readOnly

--- src/main/java/fr/Infuseting/fight/Fight.java
+++ src/main/java/fr/Infuseting/fight/Fight.java
@@ -5,11 +5,30 @@ import fr.Infuseting.entity.Monster;
 import fr.Infuseting.entity.Player;
 
 public class Fight {
-    private Player player;
-    private Monster monster;
+    private final Player player;
+    private final Monster monster;
+
+    public Fight() {
+        player = new Player();
+        monster = new Monster();
+
+
+    }
+
+
 
     public Entity turn(Spell spell){
-        return null;
+        if(spell==null) player.attack(monster);
+        else player.castSpell(spell, monster);
+
+        if(monster.isDead()) {
+            return player;
+        }
+
+        monster.attack(player);
+
+        if(player.isDead()) return monster;
+        else return null;
     }
 
     public Entity newTurn(){
