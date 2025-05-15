42cea0a7e2f5d051b64ea1418d744da29ef4e906
Matter <antoine.matter@etu.unicaen.fr>
2025-05-15 10:13:12 +0200

    [features] 1er jet de la mise en place des sorts

--- src/main/java/fr/Infuseting/fight/Heal.java
+++ src/main/java/fr/Infuseting/fight/Heal.java
@@ -1,4 +1,7 @@
 package fr.Infuseting.fight;
 
 public class Heal extends Spell{
+    public Heal(){
+        super(1,1,25,20);
+    }
 }
--- src/main/java/fr/Infuseting/fight/Shield.java
+++ src/main/java/fr/Infuseting/fight/Shield.java
@@ -1,4 +1,19 @@
 package fr.Infuseting.fight;
 
+import fr.Infuseting.entity.Entity;
+
 public class Shield extends Spell{
+    public Shield(){
+        super(5,5,25,5);
+    }
+
+    @Override
+    public boolean isSelfSpell() {
+        return true;
+    }
+
+    @Override
+    public void specificEffect(Entity entity) {
+        entity.setArmor(entity.getArmor() + amount);
+    }
 }
